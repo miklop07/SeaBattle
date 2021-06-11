d0f1620fd8a424230c13fb3594a7bd048b5a6b35
miklop <miklop07@gmail.com>
2021-06-11 11:14:36 +0300

    refactoring

--- battle/game.py
+++ battle/game.py
@@ -1,3 +1,5 @@
+"""Game module."""
+
 import pygame
 import battle.constants as constants
 from battle.log_deck import LogDeck
@@ -6,8 +8,10 @@ from battle.gamelogic import ForPlayer
 
 
 class PlayerDeck(pygame.sprite.Sprite):
-    """Class for player's game fields were all ships are shown"""
+    """Class for player's game fields were all ships are shown."""
+
     def __init__(self, x_pos, y_pos):
+        """docstring."""
         super(PlayerDeck, self).__init__()
         self.image = pygame.Surface((30 * 11, 30 * 11))
         self.image.fill(constants.WHITE)
@@ -21,11 +25,13 @@ class PlayerDeck(pygame.sprite.Sprite):
         self.nums_letters()
 
     def draw_grid(self):
+        """docstring."""
         for i in range(1, 11):
             pygame.draw.line(self.image, constants.BLACK, (30, i * 30), (30 * 11, i * 30))
             pygame.draw.line(self.image, constants.BLACK, (i * 30, 30), (30 * i, 11 * 30))
 
     def nums_letters(self):
+        """docstring."""
         letters = "ABCDEFGHIJ"
         for i in range(10):
             nums = self.font.render(str(i + 1), True, constants.BLACK)
@@ -35,6 +41,7 @@ class PlayerDeck(pygame.sprite.Sprite):
             self.image.blit(lets, (40 + i * 30, 5))
 
     def draw_ships(self, ships_list=None, killed_ships=[], show=True):
+        """docstring."""
         if not ships_list:
             return
         for ship in ships_list:
@@ -56,14 +63,14 @@ class PlayerDeck(pygame.sprite.Sprite):
                 pygame.draw.rect(self.image, color, ((x, y), (ship_width, ship_height)), width=3)
 
     def draw_dots(self, dot_set=set()):
-        """To mark all empry cells on the field"""
+        """To mark all empry cells on the field."""
         for dot in dot_set:
             x_pos = 30 * (dot[0]) + 15
             y_pos = 30 * (dot[1]) + 15
             pygame.draw.circle(self.image, constants.BLACK, (x_pos, y_pos), 5)
 
     def draw_killed(self, kill_set=set()):
-        """To mark ships which were hit"""
+        """To mark ships which were hit."""
         for block in kill_set:
             x = block[0] * 30
             y = block[1] * 30
@@ -71,14 +78,17 @@ class PlayerDeck(pygame.sprite.Sprite):
             pygame.draw.line(self.image, constants.BLACK, (x + 30, y), (x, y + 30), 5)
 
     def renew(self):
+        """docstring."""
         self.image.fill(constants.WHITE)
         self.draw_grid()
         self.nums_letters()
 
 
 class GameDeck(pygame.sprite.Sprite):
-    """docstring for GameDeck"""
+    """docstring for GameDeck."""
+
     def __init__(self):
+        """docstring."""
         super(GameDeck, self).__init__()
         self.image = pygame.Surface((30 * 13 * 2, 30 * 13))
         self.image.fill(constants.BLACK)
@@ -90,10 +100,11 @@ class GameDeck(pygame.sprite.Sprite):
 
 
 def main():
+    """docstring."""
     pygame.init()
     pygame.mixer.init()
     screen = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
-    pygame.display.set_caption("Sea Battle")
+    pygame.display.set_caption("My Game")
     clock = pygame.time.Clock()
 
     all_sprites = pygame.sprite.Group()
@@ -124,8 +135,11 @@ def main():
     pygame.font.init()
     running = True
     while running:
+        # Держим цикл на правильной скорости
         clock.tick(constants.FPS)
+        # Ввод процесса (события)
         for event in pygame.event.get():
+            # check for closing window
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
@@ -133,6 +147,9 @@ def main():
                     log_deck.scroll_up()
                 elif event.key == pygame.K_DOWN:
                     log_deck.scroll_down()
+                elif event.key == pygame.K_a:
+                    log_deck.add_record(f"Player1 A{debug_var} +")
+                    debug_var += 1
                 elif event.key == pygame.K_q:
                     running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -170,8 +187,10 @@ def main():
                                     res = "++"
                             log_deck.add_record(f"Player1 {letter}{digit}{res}")
 
+        # Обновление
         all_sprites.update()
 
+        # Отрисовка
         screen.fill(constants.WHITE)
         all_sprites.draw(screen)
         log_deck.draw(screen)
@@ -184,6 +203,7 @@ def main():
         player1_deck.draw_ships(pl.ships.ships_list, pl.killed)
         player2_deck.draw_ships(comp.ships.ships_list, comp.killed, show=False)
 
+        # После отрисовки всего, переворачиваем экран
         pygame.display.flip()
 
         if comp.turn:
